bc2919aafcf5ce194650dc965f4f42ab7b2383b4
Julia Emily Gonzaga <shaow.1230@gmail.com>
2019-09-19 14:08:33 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ $('#other-title').hide();
 $('#title').on('change', function(){
     if ($(this).val() === 'other'){
         $('#other-title').show().focus();   
-    }; 
-    if ($(this).val() !== 'other'){
+    } else {
         $('#other-title').hide();
     };
 });
@@ -13,18 +12,16 @@ $('#design').on('change', function(){
     const $allTheme = $('#color option');
     const $jsPuns = $('#color option:lt(3)');
     const $heartJs = $('#color option:gt(2)');
-    $('#color option').removeAttr('selected');
+    $($allTheme).removeAttr('selected');
         if ($selectTheme === 'js puns'){
             $('#color option:eq(0)').attr('selected', true);
             $($jsPuns).show();
             $($heartJs).hide();
-        }; 
-        if ($selectTheme === 'heart js'){
+        } else if ($selectTheme === 'heart js'){
             $('#color option:eq(3)').attr('selected', true);
             $($heartJs).show();
             $($jsPuns).hide();
-        };
-        if ($selectTheme === 'Select Theme'){
+        } else {
             $('#color option:eq(0)').attr('selected', true);
             $($allTheme).show();
         };
